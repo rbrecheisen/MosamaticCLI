118af71358a61a18d11e201009600e7f35674d75
Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2025-06-20 10:44:30 +0200

    Updated deployment script to bump version

--- scripts/deploy2pypi.bat
+++ scripts/deploy2pypi.bat
@@ -2,6 +2,16 @@
 
 setlocal
 
+poetry version patch
+
+FOR /F %%v IN ('poetry version --short') DO SET VERSION=%%v
+echo Deploying version %VERSION% to PyPI...
+set /p CONFIRM="Is this correct? (y/n) "
+if /I NOT "%CONFIRM%"=="y" (
+    echo Aborting deployment
+    exit /b 1
+)
+
 set /p TOKEN=<C:\Users\r.brecheisen\pypi-token.txt
 
 poetry build
