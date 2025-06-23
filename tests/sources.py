8ac3a923c898ca0dfe76a20c58d5558e002e7241
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2025-06-23 09:02:08 +0200

    Updated test sources.py

--- tests/sources.py
+++ tests/sources.py
@@ -1,17 +1,18 @@
 import sys
 
 SOURCES = {
-    # 'mac': {
-    #     'input': '/Users/ralph/Desktop/downloads/pancreasdemo',
-    #     'model_files': '/Users/ralph/Desktop/downloads/pytorchmodelfiles/2.2',
-    #     'output': {
-    #         'DecompressDicomFilesTask': '/Users/ralph/Desktop/downloads/Mosamatic/CLI/DecompressDicomFilesTask',
-    #         'RescaleDicomFilesTask': '/Users/ralph/Desktop/downloads/Mosamatic/CLI/RescaleDicomFilesTask',
-    #         'SegmentMuscleFatL3Task': '/Users/ralph/Desktop/downloads/Mosamatic/CLI/MuscleFatSegmentationL3Task',
-    #         'CalculateScoresTask': '/Users/ralph/Desktop/downloads/Mosamatic/CLI/CalculateScoresTask',
-    #         'CreatePngsFromSegmentationsTask': '/Users/ralph/Desktop/downloads/Mosamatic/CLI/CreatePngsFromSegmentationsTask',
-    #     }
-    # },
+    'mac': {
+        'input': '/Users/ralph/Library/CloudStorage/GoogleDrive-ralph.brecheisen@gmail.com/My Drive/data/Mosamatic/testdata/L3',
+        'model_files': '/Users/ralph/Library/CloudStorage/GoogleDrive-ralph.brecheisen@gmail.com/My Drive/data/Mosamatic/models/pytorch/2.2/L3',
+        'output': {
+            'DecompressDicomFilesTask': '/Users/ralph/Desktop/downloads/Mosamatic/CLI/DecompressDicomFilesTask',
+            'RescaleDicomFilesTask': '/Users/ralph/Desktop/downloads/Mosamatic/CLI/RescaleDicomFilesTask',
+            'SegmentMuscleFatL3Task': '/Users/ralph/Desktop/downloads/Mosamatic/CLI/MuscleFatSegmentationL3Task',
+            'CalculateScoresTask': '/Users/ralph/Desktop/downloads/Mosamatic/CLI/CalculateScoresTask',
+            'CreatePngsFromSegmentationsTask': '/Users/ralph/Desktop/downloads/Mosamatic/CLI/CreatePngsFromSegmentationsTask',
+            'DefaultPipeline': '/Users/ralph/Desktop/downloads/Mosamatic/CLI/DefaultPipeline',
+        }
+    },
     'windows': {
         'input': 'G:\\My Drive\\data\\Mosamatic\\testdata\\L3',
         'model_files': 'G:\\My Drive\\data\\Mosamatic\\models\\pytorch\\2.2\\L3',
@@ -21,6 +22,7 @@ SOURCES = {
             'SegmentMuscleFatL3Task': 'D:\\Mosamatic\\CLI\\Output\\MuscleFatSegmentationL3Task',
             'CalculateScoresTask': 'D:\\Mosamatic\\CLI\\Output\\CalculateScoresTask',
             'CreatePngsFromSegmentationsTask': 'D:\\Mosamatic\\CLI\\Output\\CreatePngsFromSegmentationsTask',
+            'DefaultPipeline': 'D:\\Mosamatic\\CLI\\Output\\DefaultPipeline',
         }
     }
 }
