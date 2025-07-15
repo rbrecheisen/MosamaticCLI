5e5f13515f2ca0a8b6df9604a553e304fcfdd545
Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2025-07-15 12:49:19 +0200

    Saving pyproject.toml

--- mosamatic/logging.py
+++ mosamatic/logging.py
@@ -4,11 +4,13 @@ import datetime
 class LogManager:
     def __init__(self):
         self._name = 'mosamatic-cli'
+        self._listeners = []
 
     def _log(self, level, message):
         timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         message = f'[{timestamp}] {level} {self._name}: {message}'
         print(message)
+        self.notify_listeners(message)
 
     def info(self, message):
         self._log('INFO', message)
@@ -18,3 +20,11 @@ class LogManager:
 
     def error(self, message):
         self._log('ERROR', message)
+
+    def add_listener(self, listener):
+        if listener not in self._listeners:
+            self._listeners.append(listener)
+
+    def notify_listeners(self, message):
+        for listener in self._listeners:
+            listener.new_message(message)
