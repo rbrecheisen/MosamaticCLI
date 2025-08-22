ae57fe76edbcbe6e409ada4b34303a839a203115
Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2025-08-22 14:18:14 +0200

    Saving pyproject.toml

--- scripts/updatetoml.bat
+++ scripts/updatetoml.bat
@@ -1,6 +1,10 @@
 @echo off
 
+copy /Y pyproject.toml.windows pyproject.toml
+
 for /f "usebackq tokens=* delims=" %%a in ("requirements-win.txt") do (
     echo %%a
     poetry add %%a
 )
+
+copy /Y pyproject.toml pyproject.toml.windows
